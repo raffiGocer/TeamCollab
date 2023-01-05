d9a01a2a3f5f075f65bde099cae636d64ba695c6
fatihalpozdemir <ozdemir.fatihalp@gmail.com>
2023-01-05 02:52:33 -0500

    fatih s message

---
+++ src/utils/Test2.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Fatih ");
+        System.out.println("sorry for the meeting guys , I had to work so I could not join to meeting ");
+    }
+}
